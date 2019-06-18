3ae1923307b7e25271abc841151803a487b0cd18
gkennedy87 <gkennedy87@gmail.com>
2019-06-18 09:08:17 +0900

    first migration
    -Created first DB migration to create table columns for app.
    -Updated controllers with empty crud functions.
    
    - Created layout view and index view for app then tested.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('tasks','TaskController@index');
+Route::post('tasks','TaskController@store');
+Route::delete('tasks/{id}','TaskController@delete');
